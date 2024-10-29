960fb5fd59d5a20850de5a4808efb015516c9c83
AsHtrich <amoghash80@gmail.com>
2024-10-29 20:17:28 +0530

    Change are madee

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => res.send('Hello from Blue-Green Deployment! Changes are made!'));
+app.get('/', (req, res) => res.send('Hello from Blue-Green Deployment! Changes are made!!'));
 
 app.listen(PORT, () => console.log(`App running on http://localhost:${PORT}`));
